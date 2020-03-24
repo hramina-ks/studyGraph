7a40fcc9efa23130d604cb73e4125fef808b408b
Ксения Храмина <hramina-ks@hotmail.com>
2020-03-24 19:56:16 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace studyGraph
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.Line = new System.Windows.Forms.ToolStripButton();
+            this.Rectangle = new System.Windows.Forms.ToolStripButton();
+            this.Pencil = new System.Windows.Forms.ToolStripButton();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.colorsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Image = global::studyGraph.Properties.Resources.icons8_главная_16;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.Image = global::studyGraph.Properties.Resources.icons8_файл_16;
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.createToolStripMenuItem.Text = "Создать";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Image = global::studyGraph.Properties.Resources.icons8_сохранить_16;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.saveToolStripMenuItem.Text = "Сохранить";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(132, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Image = global::studyGraph.Properties.Resources.icons8_знак_выхода_16;
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitToolStripMenuItem.Text = "Выйти";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Image = global::studyGraph.Properties.Resources.icons8_редактировать_16;
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.editToolStripMenuItem.Text = "Правка";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Image = global::studyGraph.Properties.Resources.icons8_головоломка_161;
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.viewToolStripMenuItem.Text = "Вид";
+            // 
+            // colorsToolStripMenuItem
+            // 
+            this.colorsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editingToolStripMenuItem});
+            this.colorsToolStripMenuItem.Image = global::studyGraph.Properties.Resources.icons8_палитра_16;
+            this.colorsToolStripMenuItem.Name = "colorsToolStripMenuItem";
+            this.colorsToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.colorsToolStripMenuItem.Text = "Цвета";
+            // 
+            // editingToolStripMenuItem
+            // 
+            this.editingToolStripMenuItem.Image = global::studyGraph.Properties.Resources.icons8_rgb_круг_2_16;
+            this.editingToolStripMenuItem.Name = "editingToolStripMenuItem";
+            this.editingToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.editingToolStripMenuItem.Text = "Редактировать...";
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Line,
+            this.Rectangle,
+            this.Pencil});
+            this.toolStrip.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(784, 25);
+            this.toolStrip.TabIndex = 2;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // Line
+            // 
+            this.Line.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Line.Image = global::studyGraph.Properties.Resources.icons8_линия_50;
+            this.Line.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Line.Name = "Line";
+            this.Line.Size = new System.Drawing.Size(23, 22);
+            this.Line.Text = "Line";
+            this.Line.Click += new System.EventHandler(this.Tool_Click);
+            // 
+            // Rectangle
+            // 
+            this.Rectangle.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Rectangle.Image = global::studyGraph.Properties.Resources.icons8_прямоугольник_16__1_;
+            this.Rectangle.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Rectangle.Name = "Rectangle";
+            this.Rectangle.Size = new System.Drawing.Size(23, 22);
+            this.Rectangle.Text = "Rectangle";
+            this.Rectangle.Click += new System.EventHandler(this.Tool_Click);
+            // 
+            // Pencil
+            // 
+            this.Pencil.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Pencil.Image = global::studyGraph.Properties.Resources.icons8_кончик_карандаша_16;
+            this.Pencil.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Pencil.Name = "Pencil";
+            this.Pencil.Size = new System.Drawing.Size(23, 22);
+            this.Pencil.Text = "Pencil";
+            this.Pencil.Click += new System.EventHandler(this.Tool_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(784, 537);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "Графический редактор";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editingToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton Line;
+        private System.Windows.Forms.ToolStripButton Rectangle;
+        private System.Windows.Forms.ToolStripButton Pencil;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studyGraph
+{
+    public partial class FormMain : Form
+    {
+        bool isPressed = false;
+        int x1, y1, x2, y2; //координаты мыши
+        Bitmap snapshot, tempDraw; //снимки экрана
+        Color foreColor; //цвет
+        int lineWidth; //ширина линий
+        string selectedTool; 
+
+        public FormMain()
+        {
+            InitializeComponent();
+            snapshot = new Bitmap(pictureBox.ClientRectangle.Width, pictureBox.ClientRectangle.Height);
+            tempDraw = (Bitmap)snapshot.Clone();
+            ForeColor = Color.Black;
+            lineWidth = 2;
+            Pencil.Checked = true;
+            selectedTool = "Pencil";
+        }
+
+        private void Tool_Click(object sender, EventArgs e)
+        {
+            Line.Checked = false;
+            Rectangle.Checked = false;
+            Pencil.Checked = false;
+            ToolStripButton btnClicked = sender as ToolStripButton;
+            btnClicked.Checked = true;
+            selectedTool = btnClicked.Name;
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPressed = true;
+            x1 = e.X;
+            y1 = e.Y;
+            tempDraw = (Bitmap)snapshot.Clone();
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
+            snapshot = (Bitmap)tempDraw.Clone();
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed)
+            {
+                x2 = e.X;
+                y2 = e.Y;
+                pictureBox.Invalidate();
+                pictureBox.Update();
+            }
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (selectedTool != "Pencil")
+                tempDraw = (Bitmap)snapshot.Clone();
+            Graphics g = Graphics.FromImage(tempDraw);
+            Pen myPen = new Pen(foreColor, lineWidth);
+            switch (selectedTool)
+            {
+                case "Line":
+                    if (tempDraw != null)
+                        g.DrawLine(myPen, x1, y1, x2, y2);
+                    break;
+                case "Rectangle":
+                    if (tempDraw != null)
+                        g.DrawRectangle(myPen, x1, y1, x2 - x1, y2 - y1);
+                    break;
+                case "Pencil":
+                    if (tempDraw != null)
+                    {
+                        g.DrawLine(myPen, x1, y1, x2, y2);
+                        x1 = x2;
+                        y1 = y2;
+                    }
+                    break;
+                default: break;
+            }
+            myPen.Dispose();
+            e.Graphics.DrawImageUnscaled(tempDraw, 0, 0);
+            g.Dispose();
+        }
+    }
+}
